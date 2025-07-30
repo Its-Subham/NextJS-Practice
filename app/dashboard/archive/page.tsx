8d99efd65e94c3adcfe65feda7f9f5f2b3884051
Its-Subham <subhambera80062@gmail.com>
2025-07-30 11:25:43 +0530

    Add Parallel Routes

---
+++ app/dashboard/archive/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function Archive() {
+  return (
+    <div>
+      <h1>Archive Notification Page</h1>
+      <Link href="/dashboard"> Dashboard</Link>
+    </div>
+  );
+}
